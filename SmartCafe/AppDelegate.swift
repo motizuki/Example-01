fcb9298e909b4f4a34d3deb72c20f72aa771e443
motizuki <gustavokm90@gmail.com>
2016-01-19 23:09:18 +1300

    Adding top buttons

--- SmartCafe/AppDelegate.swift
+++ SmartCafe/AppDelegate.swift
@@ -21,11 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         ) -> Bool {
 
         UINavigationBar.appearance().tintColor = Palette.color6
-        // Sets background to a blank/empty image
-//        UINavigationBar.appearance().setBackgroundImage(UIImage(), forBarMetrics: .Default)
-        // Sets shadow (line below the bar) to a blank image
-//        UINavigationBar.appearance().shadowImage = UIImage()
-        // Set translucent. (Default value is already true, so this can be removed if desired.)
+        UINavigationBar.appearance().barTintColor = Palette.color5
         UINavigationBar.appearance().translucent = true
         let font = UIFont(name: "Futura-Medium", size: 18)!
         let uiBarButtonAttrb = [
